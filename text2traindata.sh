49381f5a39d38544575326bee8690a88acfb13d4
initega <initega@gmail.com>
2019-10-12 20:09:07 +0200

    trim on script

--- text2traindata.sh
+++ text2traindata.sh
@@ -16,6 +16,7 @@ content="$(normalize "$file")"
 echo "$content" | while read -r line
 do
     awk_command="{print match(\$0, \"$name\")}"
+    line="$(echo "$line" | xargs)"
     text="\"$line\""
 
     location="$(echo "$text" | awk "$awk_command")"
