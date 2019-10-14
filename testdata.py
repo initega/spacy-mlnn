9b8edf72ee2820ef54ba3d0d124edb823c693517
initega <initega@gmail.com>
2019-10-14 12:48:32 +0200

    model1: blank + ML. modelTest: predefined + ML

--- testdata.py
+++ testdata.py
@@ -1,12 +1,4 @@
 test_text = \
 """\
-Inductive logic programming is particularly useful in bioinformatics and natural
-language processing. Gordon Plotkin and Ehud Shapiro laid the initial
-theoretical foundation for inductive machine learning in a logical
-setting.[44][45][46] Shapiro built their first implementation (Model Inference
-System) in 1981: a Prolog program that inductively inferred logic programs from
-positive and negative examples.[47] The term inductive here refers to
-philosophical induction, suggesting a theory to explain observed facts, rather
-than mathematical induction, proving a property for all members of a
-well-ordered set.
+Inductive logic programming is particularly useful in bioinformatics and natural language processing. Gordon Plotkin and Ehud Shapiro laid the initial theoretical foundation for inductive machine learning in a logical setting.[44][45][46] Shapiro built their first implementation (Model Inference System) in 1981: a Prolog program that inductively inferred logic programs from positive and negative examples.[47] The term inductive here refers to philosophical induction, suggesting a theory to explain observed facts, rather than mathematical induction, proving a property for all members of a well-ordered set.
 """
